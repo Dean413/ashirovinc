1418b5961a70052ef52f800082d631790001cbcc
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-09-18 22:53:12 +0100

    change the padding-y of nav

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import FullPageLoader from "@/app/component/page-reloader";
 
 export default function AuthCallback() {
   const router = useRouter();
   const supabase = createClientComponentClient();
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const handleSession = async () => {
@@ -14,6 +16,7 @@ export default function AuthCallback() {
       const { data, error } = await supabase.auth.getSession();
 
       if (data.session) {
+        setLoading(false)
         // ✅ User is logged in → redirect to dashboard
         router.push("/dashboard/client-dashboard");
       } else {
@@ -25,9 +28,5 @@ export default function AuthCallback() {
     handleSession();
   }, [router, supabase]);
 
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p className="text-gray-600">Signing you in...</p>
-    </div>
-  );
+ if(loading) return <FullPageLoader text="signing in...."/>
 }
